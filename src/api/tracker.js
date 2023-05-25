2c39c877a2786b823b71cc712625b9bad93987da
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-05-25 15:47:08 -0400

    map working

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://b93e-108-50-194-27.ngrok-free.app'
+});
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { Text } from 'react-native-elements'
+import Map from '../components/Map';
+
+const TrackCreateScreen = () => {
+    return (
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h2>Create a Track</Text>
+            <Map />
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreateScreen;
